93cedf191fb8a1197ce2b1fcf7a316b22c47a219
gaopeng029 <45783599+gaopeng029@users.noreply.github.com>
2019-12-23 17:58:27 +0800

    3
    3

--- demo1/src/demo1/Person.java
+++ demo1/src/demo1/Person.java
@@ -2,4 +2,17 @@ package demo1;
 
 public class Person {
 	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [name=" + name + "]";
+	}
 }
